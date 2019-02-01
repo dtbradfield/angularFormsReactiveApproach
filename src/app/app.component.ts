e0b7d626de4d698a9e2ccd78a2603b04d11da97c
DonTheChimera <donaldtbradfield@gmail.com>
2019-02-01 14:03:37 -0600

    used reactiveformsmodule to programmatically hook up a form and submit it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit {
+  genders = ['male', 'female', 'other'];
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('other')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
 }
